9534d42d333a676377a2368ae46fa429c62ea003
Chris Rink <chrisrink10@gmail.com>
2018-12-05 11:25:09 -0500

    Day 4, part 2

--- src/aoc_2018/day4.clj
+++ src/aoc_2018/day4.clj
@@ -73,5 +73,64 @@
         ((fn [{:keys [id asleep-duration mins-asleep]}]
            (* (Integer/parseInt id) (key (apply max-key val mins-asleep))))))))
 
+(defn part2
+  ([]
+   (part2 "day4/input.txt"))
+  ([input-filename]
+   (->> (io/resource input-filename)
+        (slurp)
+        (str/split-lines)
+        (map (fn [line]
+               (let [[_ year month day hour minute msg :as v] (re-matches input-pattern line)
+
+                     date [(Integer/parseInt year)
+                           (Integer/parseInt month)
+                           (Integer/parseInt day)
+                           (Integer/parseInt hour)
+                           (Integer/parseInt minute)]]
+                 [date msg])))
+        (into (sorted-map))
+        (partition-by (guard-partition))
+        (map (fn [logs]
+               (let [[_ guard-id] (->> (first logs)
+                                       (second)
+                                       (re-matches guard-msg-pattern))]
+                 [guard-id
+                  (->> (drop 1 logs)
+                       (map (fn [[[_ _ _ _ minute] msg]]
+                              (condp = msg
+                                "falls asleep" [:fell-asleep minute]
+                                "wakes up"     [:woke-up minute])))
+                       (sort-by second)
+                       (reduce (fn [m [event-type minute]]
+                                 (if (= event-type :fell-asleep)
+                                   (assoc m :fell-asleep-min minute)
+                                   (let [fell-asleep-min  (:fell-asleep-min m)
+                                         total-asleep-min (- minute fell-asleep-min)
+                                         mins-asleep      (concat (:mins-asleep m) (range fell-asleep-min minute))]
+                                     (-> m
+                                         (update :asleep-duration #(cond-> total-asleep-min % (+ %)))
+                                         (dissoc :fell-asleep-min)
+                                         (assoc :mins-asleep mins-asleep)))))
+                               {}))])))
+        (reduce (fn [m [id {:keys [asleep-duration mins-asleep] :as details}]]
+                  (->> (fn [{prev-asleep-duration :asleep-duration prev-mins-asleep :mins-asleep}]
+                         {:id              id
+                          :asleep-duration (+ (or prev-asleep-duration 0) (or asleep-duration 0))
+                          :mins-asleep     (reduce (fn [m minute]
+                                                     (update m minute #(if % (inc %) 1)))
+                                                   (or prev-mins-asleep {})
+                                                   mins-asleep)})
+                       (update m id)))
+                {})
+        (vals)
+        (sort-by (fn [{:keys [mins-asleep] :as d}]
+                   (if-let [asleep-times (vals mins-asleep)]
+                     (- (apply max asleep-times))
+                     0)))
+        (first)
+        ((fn [{:keys [id asleep-duration mins-asleep]}]
+           (* (Integer/parseInt id) (key (apply max-key val mins-asleep))))))))
+
 
 
